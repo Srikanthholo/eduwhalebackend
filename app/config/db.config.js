89b864be796a4d049deb5375d2aa87d27cbee391
Srikanthholo <101865042+Srikanthholo@users.noreply.github.com>
2023-10-30 09:55:48 +0530

    config

---
+++ app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  murl:"mongodb+srv://sri2627:Rama$9897@cluster0.fatnc.mongodb.net/students",
+  url: "mongodb+srv://sri2627:Rama$9897@cluster0.fatnc.mongodb.net/",
+  database: "students",
+  imgBucket: "photos",
+};
+ 
