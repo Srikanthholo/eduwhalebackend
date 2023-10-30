3cf74e929293f9e8ede869353b986e67ac6dde5a
Srikanthholo <101865042+Srikanthholo@users.noreply.github.com>
2023-10-30 09:58:49 +0530

    Delete config directory

--- config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  murl:"mongodb+srv://sri2627:Rama$9897@cluster0.fatnc.mongodb.net/students",
-  url: "mongodb+srv://sri2627:Rama$9897@cluster0.fatnc.mongodb.net/",
-  database: "students",
-  imgBucket: "photos",
-};
- 
